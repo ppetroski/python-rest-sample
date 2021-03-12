5e27d1f406d289e398f72ce820e05c73df9fa684
Paul Petroski <paul.petroski@hotmail.com>
2021-03-12 18:22:16 -0500

    Pagination

--- components/framework/baseController.py
+++ components/framework/baseController.py
@@ -40,38 +40,30 @@ class BaseController(Resource):
 
     def get(self, uuid=None):
         if self._model != None:
-            # include Joins for relations requested via expand parameter
-            relations = request.args.getlist('expand')
-            for relation in relations:
-                if hasattr(self._model, relation):
-                    self._query = self._query.options(
-                        joinedload(getattr(self._model, relation))
-                    )
-
             # Return a collection of existing models or a single model.
             if uuid is None:
-                data = self._query.all()
-                return self._send(data)
+                return self._paginate()
             else:
-                data = self._query.filter_by(uuid=uuid).all()
-                return self._send(data)
+                self._query = self._query.filter_by(uuid=uuid)
+                return self._paginate()
         else:
-            return self._send('The API endpoint has been improperly configured. Please contact support.', 'failure')
+            return self._message('The API endpoint has been improperly configured. Please contact support.', 'failure')
 
     def delete(self, uuid=None):
         if self._model != None:
             if uuid:
-                data = self._query.filter_by(uuid=uuid).first()
+                self._query = self._query.filter_by(uuid=uuid)
+                data = self._query.first()
                 if data and data.id:
                     # I believe in soft deletes, so we'll do an update
                     data.deleted_at = datetime.now()
                     self._session.commit()
-                    return self._send(data)
+                    return self._paginate()
                 else:
                     model = type(self._model()).__name__
-                    return self._send(f'"{model}" record not found for "{uuid}"', 'failure')
+                    return self._message(f'"{model}" record not found for "{uuid}"', 'failure')
         else:
-            return self._send('The API endpoint has been improperly configured. Please contact support.', 'failure')
+            return self._message('The API endpoint has been improperly configured. Please contact support.', 'failure')
 
     def post(self, uuid=None, init=None):
         # Create and save a new model
@@ -79,55 +71,63 @@ class BaseController(Resource):
             if self._model != None:
                 post = request.get_json()
                 if uuid:
-                    data = self._query.filter_by(uuid=uuid).first()
+                    self._query = self._query.filter_by(uuid=uuid)
+                    data = self._query.first()
                     if data and data.id:
                         data.load(post)
                     else:
                         model = type(self._model()).__name__
-                        return self._send(f'"{model}" record not found for "{uuid}"', 'failure')
+                        return self._message(f'"{model}" record not found for "{uuid}"', 'failure')
                 else:
                     data = self._model(post)
                     self._session.add(data)
                 self._session.commit()
-                return self._send(data)
+                return self._paginate()
             else:
-                return self._send('The API endpoint has been improperly configured. Please contact support.', 'failure')
+                return self._message('The API endpoint has been improperly configured. Please contact support.', 'failure')
         except ValueError as message:
             self._session.rollback()
             self._session.rollback()
-            return self._send(str(message), 'failure')
+            return self._message(str(message), 'failure')
         except:
             self._session.rollback()
             error = ''
             for ele in sys.exc_info():
                 error += str(ele)
             logger.error(error)
-            return self._send('Unexpected error: ' + str(sys.exc_info()[1]), 'failure')
+            return self._message('Unexpected error: ' + str(sys.exc_info()[1]), 'failure')
 
     def put(self):
         return self.post(self)
 
-    # TODO: Add functions for page size, pagination, etc...
+    def _paginate(self):
+        # include Joins for relations requested via expand parameter
+        relations = request.args.getlist('expand')
+        for relation in relations:
+            if hasattr(self._model, relation):
+                self._query = self._query.options(
+                    joinedload(getattr(self._model, relation))
+                )
+
+        count = self._query.count()
+
+        # Perform Pagination
+        page = request.args.get('page', default=1, type=int)
+        per_page = request.args.get('per-page', default=25, type=int)
+        self._query = self._query.limit(per_page).offset((page-1) * per_page)
+
+        # Query Data and serialize
+        data = self._query.all()
+        if isinstance(data, list):
+            data = [m.serialize(relations) for m in data]
+        else:
+            data = data.serialize(relations)
+        return make_response(jsonify({'status': 'success', 'count': count, 'timestamp': time.time(), 'data': data}))
 
     @staticmethod
-    def _send(data, status='success'):
+    def _message(data, status='success'):
         if status != 'success':
             if (isinstance(data, str)):
                 logger.warning(data)
 
-        # include any relation models requested via expand parameter
-        relations = request.args.getlist('expand')
-
-        if data is None:
-            count = 0
-        elif isinstance(data, str):
-            data = data
-            count = 0
-        elif isinstance(data, list):
-            count = len(data)
-            data = [m.serialize(relations) for m in data]
-        else:
-            count = 1
-            data = data.serialize(relations)
-
-        return make_response(jsonify({'status': status, 'count': count, 'timestamp': time.time(), 'data': data}))
+        return make_response(jsonify({'status': status, 'count': 0, 'timestamp': time.time(), 'data': data}))
